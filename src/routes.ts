66c0f841641cd7cdca37cedbd9d9f6fec396e2f9
Daniel Cunha <danielcc.jp@gmail.com>
2020-10-29 10:31:10 -0300

    tipando objetos e vetores

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,25 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
+
+// string, number, boolean, object, Array
+// interfaces
 
 export function helloWorld(request: Request, response: Response) {
-  return response.json({ messsage: 'Hello World' });
+  const user = createUser({
+    email: 'danielcc.jp@gmail.com',
+    password: '123456',
+    techs: [
+      'Node.JS',
+      'ReactJS',
+      'React Native',
+      {
+        title: 'Javascript',
+        experience: 100,
+      },
+    ],
+  });
+
+  console.log(user.email);
+
+  return response.json({ message: 'Hello World' });
 }
